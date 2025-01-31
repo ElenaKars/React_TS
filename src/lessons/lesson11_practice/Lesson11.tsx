b96c671c36a1f8f505b4d5b052ae25dcc44e417f
ElenaKars <elena_baldina@yahoo.com>
2025-01-31 09:49:06 +0100

    lesson11_practice

--- src/lessons/lesson11_practice/Lesson11.tsx
+++ src/lessons/lesson11_practice/Lesson11.tsx
@@ -1,9 +1,70 @@
-import { Lesson11Wrapper } from "./styles";
+import { ChangeEvent, useEffect, useState } from "react";
+import Button from "../../components/Button/Button";
+import { ButtonWrapper, FactCard, InfoContainer, Lesson11Wrapper } from "./styles";
+import axios from "axios";
+import Spinner from "../../components/Spinner/Spinner";
+import { ErrorBlock } from "../lesson10/styles";
 
 function Lesson11() {
+  const URL_DATA: string = 'https://catfact.ninja/fact';
+  const [facts, setFact] = useState<string[]>([]);
+  const [isVisible, setVisible] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<undefined | string>(undefined);
+  const [disabled, setDisabled] = useState<boolean>(false);
+
+  const factCards = facts.map((fact, i) => {
+    return (
+      <><FactCard key={i} isNew={i === facts.length - 1}>{fact}</FactCard></>
+    );
+  });
+
+  const deleteAll = () => {
+    setFact([]);
+    setVisible(false);
+  };
+
+  const getCatsFact = async () => {
+    setError(undefined);
+    if (facts.length === 0) {
+      setLoading(true);
+    }
+    setDisabled(true);
+    // setVisible(false);
+    try {
+      const result = await axios.get(URL_DATA);
+      const data = result.data.fact;
+      setFact((facts) => [...facts, data]);
+      setVisible(true);
+    } catch (error: any) {
+      setError(error.massage);
+    } finally {
+      setLoading(false);
+      setDisabled(false);
+    }
+  };
+
+  useEffect(() => {
+    if (facts.length === 0) {
+
+      getCatsFact();
+    }
+  }, []);
+
   return (
     <Lesson11Wrapper>
-      /* content here */
+      <ButtonWrapper>
+        <Button name="GET MORE INFO" onClick={getCatsFact} disabled={disabled} />
+      </ButtonWrapper>
+
+      {loading && <Spinner />}
+
+      {error && <ErrorBlock />}
+      {facts && <InfoContainer isVisible={isVisible}>{factCards}
+      </InfoContainer>}
+      <ButtonWrapper>
+        <Button name="DELETE ALL DATA" onClick={deleteAll} />
+      </ButtonWrapper>
     </Lesson11Wrapper>
   );
 }
--- src/lessons/lesson11_practice/styles.ts
+++ src/lessons/lesson11_practice/styles.ts
@@ -1,5 +1,12 @@
 import styled from "@emotion/styled";
 
+interface InfoContainerProps {
+  isVisible: boolean;
+}
+interface NewFactCard {
+  isNew?: boolean;
+}
+
 export const Lesson11Wrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -7,4 +14,80 @@ export const Lesson11Wrapper = styled.div`
   padding: 40px;
   flex: 1;
   gap: 20px;
+  justify-content: space-between;
+`;
+
+export const ButtonWrapper = styled.div`
+  width: 800px;
+`;
+
+export const InfoContainer = styled.div<InfoContainerProps>`
+  padding: 30px;
+  display: ${({ isVisible }) => (isVisible ? "flex" : "none")};
+  flex-wrap: wrap;
+  max-height: 700px;
+  overflow-y: scroll;
+  width: 800px;
+  overflow-y: auto;
+  background-color: #8acae6;
+  border: 1px solid black;
+  border-radius: 6px;
+  flex: 1;
+  align-items: flex-start;
+  justify-content: center;
+
+  &::-webkit-scrollbar {
+    width: 8px; /* Толщина полосы прокрутки */
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #888; /* Цвет ползунка */
+    border-radius: 4px;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background-color: #555; /* Цвет при наведении */
+  }
+
+  &::-webkit-scrollbar-track {
+    background: #f1f1f1; /* Цвет фона полосы */
+  }
+`;
+
+export const FactCard = styled.div<NewFactCard>`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 15px;
+  background-color: aliceblue;
+  width: 25%;
+  min-height: 100px;
+  height: auto;
+  border: 1px solid grey;
+  border-radius: 4px;
+  margin: 10px;
+  word-wrap: break-word;
+  white-space: normal;
+  overflow: hidden;
+  /* opacity: 0; */
+  transform: translateY(10px);
+
+  ${({ isNew }) =>
+    isNew &&
+    `
+    opacity: 0;
+    transform: translateY(10px);
+    animation: fadeIn 0.5s ease-in-out forwards;
+  `}
+
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+      transform: translateY(10px);
+    }
+    to {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
 `;
