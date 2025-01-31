a9b44523dd6fbf8f0a0ad9bdcb66578d28a151f4
ElenaKars <elena_baldina@yahoo.com>
2025-01-31 21:57:03 +0100

    homework12

---
+++ src/homeworks/homework12/Homework12.tsx
@@ -0,0 +1,11 @@
+import { Homework12Wrapper } from "./styles";
+
+function Homework12() {
+    return (
+        <Homework12Wrapper>
+
+        </Homework12Wrapper>
+    );
+}
+
+export default Homework12;
---
+++ src/homeworks/homework12/styles.ts
@@ -0,0 +1,5 @@
+import styled from "@emotion/styled";
+
+export const Homework12Wrapper = styled.div`
+  /* styles here */
+`;
